dba5a2bff86e425d1e51a691a46ce416426bc2a0
NAUFAL ABRAR RABBANI <87893692+naufalabrarrabbani@users.noreply.github.com>
2023-12-29 03:57:21 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin');
 module.exports = {
-  content: ['./index.html'],
+  content: ['./*.html'],
   theme: {
     extend: {
       colors: {
